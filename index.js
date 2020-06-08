e89e70bc454e41ba26995578a9f117e0c1030a0c
John K. Speck <speckworks@gmail.com>
2020-06-08 20:09:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+arraySuits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+arrayValues = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+var deck = {};
+
+for(let y = 0; y < arraySuits.length; y++){
+    deck[y] = 
+}
+console.log(deck)
+}
