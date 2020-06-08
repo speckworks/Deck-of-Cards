784e4a8ddc16bf9464bb8f0639cbc92ee4f52ec6
John K. Speck <speckworks@gmail.com>
2020-06-08 21:22:26 +0000

    Now it is able to sort by Card Value

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let suits = ["spades", "diamonds", "clubs", "hearts"];
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let deck = new Array();
 
-function getDeck()
-{
-	var deck = new Array();
+const getDeck = () => {
 
-	for(var i = 0; i < suits.length; i++)
+	for(let i = 0; i < suits.length; ++i)
 	{
-		for(var x = 0; x < values.length; x++)
+		for(let j = 0; j < values.length; ++j)
 		{
-			var card = {Value: values[x], Suit: suits[i]};
+			let card = { 
+        key: values[j], value: suits[i]
+        };
 			deck.push(card);
 		}
 	}
@@ -18,3 +19,35 @@ function getDeck()
 }
 
 getDeck()
+// console.log(deck)
+
+function shuffle(deck)
+{
+	// for 1000 turns
+	// switch the values of two random cards
+	for (var i = 0; i < 1000; i++)
+	{
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+}
+
+shuffle(deck);
+
+deck.sort(function(a, b) {
+  var nameA = a.value.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.value.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
